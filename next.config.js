9feebf2200ed40cead8c6007455d6a1fd5907bf0
jzohdi <31998568+jzohdi@users.noreply.github.com>
2021-01-30 20:43:26 -0500

    tried to optimize css but size is coming from firebase

--- next.config.js
+++ next.config.js
@@ -4,6 +4,32 @@ const withPurgeCss = require("next-purgecss");
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: eval(process.env.ANALYZE) === true,
 });
-module.exports = withBundleAnalyzer({});
+module.exports = withBundleAnalyzer({
+  plugins: [
+    "postcss-flexbugs-fixes",
+    [
+      "postcss-preset-env",
+      {
+        autoprefixer: {
+          flexbox: "no-2009",
+        },
+        stage: 3,
+        features: {
+          "custom-properties": false,
+        },
+      },
+    ],
+    [
+      "@fullhuman/postcss-purgecss",
+      {
+        content: [
+          "./pages/**/*.{js,jsx,ts,tsx}",
+          "./components/**/*.{js,jsx,ts,tsx}",
+        ],
+        defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+      },
+    ],
+  ],
+});
 
 // module.exports = withCss(withPurgeCss());
